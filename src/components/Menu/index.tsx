689a6d42b7db95379f935581c81a97e812305482
nerdfinance2022 <99462770+nerdfinance2022@users.noreply.github.com>
2022-03-14 11:26:55 +0530

    Add files via upload

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,7 +4,7 @@ import { allLanguages } from 'config/localisation/languageCodes'
 import { LanguageContext } from 'contexts/Localisation/languageContext'
 import useTheme from 'hooks/useTheme'
 import { usePriceCakeBusd } from 'state/hooks'
-import { Menu as UikitMenu } from 'nerdfinancev2'
+import { Menu as UikitMenu } from 'platinumfinancev2'
 import config from './config'
 
 const Menu = (props) => {
